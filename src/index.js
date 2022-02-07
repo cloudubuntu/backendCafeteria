b5fccf6f2633a3b6c088ee5229405722a6160b65
alanChibilisco <alan.chibilisco@gmail.com>
2022-02-07 18:27:10 -0300

    se instalo dependencias

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('prueba backend-alan');
+import express from 'express';
+
+
+//crear una instancia de express
+const app =express();
+//configuracion del puerto
+app.set('port', process.env.PORT||4000);//toma el valor desde la variable de entorno (env) como PORT or puerto 4000 o el de mi eleccion
+
+app.listen(app.get('port'), ()=>{console.log('Ejecutandose en el puerto:'+app.get('port'));});
+//prueba del backend
+//console.log('prueba backend-alan');
