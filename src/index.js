bdfa798fd2fe1fd783213eed4aeebade7eac118d
alanChibilisco <alan.chibilisco@gmail.com>
2022-02-12 20:14:45 -0300

    guardado

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import express from 'express';
-
+import res from 'express/lib/response';
+import morgan from 'morgan';
+import cors from 'cors';
+import  path  from 'path';
 
 //crear una instancia de express
 const app =express();
@@ -9,3 +12,31 @@ app.set('port', process.env.PORT||4000);//toma el valor desde la variable de ent
 app.listen(app.get('port'), ()=>{console.log('Ejecutandose en el puerto:'+app.get('port'));});
 //prueba del backend
 //console.log('prueba backend-alan');
+
+//midlewares: config extras del backend antes de que se ejecuten las rutas
+//1-midle nativos de express
+app.use(express.json()); //permite recibir objetos en formato json
+app.use(express.urlencoded({extended:true})); //
+
+//cargar archivo static
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+//pagina por defecto al cargar la URL del backend
+console.log("esta es mi prueba del backend donde puedo enviar un lista de productos");
+console.log(__dirname);
+console.log(path.join(__dirname, '../public'));
+
+
+//2-midle externos npm install morgan cors, primero instalar luego importar, morgan agrega detalles a la terminal va con parametros, cors habilita el backend para que reciba peticiones remotas
+app.use(morgan('dev')); //detalles en termina
+app.use(cors()); //peticiones remotas (remote request)
+
+//creamos una ruta de prueba
+//GET-pedir, POST-crear, PUT-actualizar, DELETE-borrar
+app.get("/products", (req, res)=>{
+    //lleva 2 parametros, 1 consulta (request), 2 respuesta (response)
+    //aqui se ecribe la logica que se ejhecuta cuando llega un request
+    console.log("esta es mi prueba del backend donde puedo enviar un lista de productos");
+    res.send("asi se muestra en el navegador-ahora con acutalizacion");
+});
