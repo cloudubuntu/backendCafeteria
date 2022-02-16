bb3417ff8bb58fa1db766fe510e15684244ba3bc
alanChibilisco <alan.chibilisco@gmail.com>
2022-02-16 18:39:44 -0300

    se completo el get y el post

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,18 +1,47 @@
 //aqui va toda la logica
+import Producto from "../models/producto";
 
+const productoCtrl = {};
 
+productoCtrl.listarProductos = async (req, res) => {
+    //cargar la logica para traer los productos de la BD
+    try {
+        //buscar en la BBDD la coleccion de productos
+        const listaProducts=await Producto.find();
+        //enviar la respuesta al FrontEnd
+        res.status(200).json(listaProducts);
+        
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({
+            mensaje: "Error al intentar listar los productos producto",
+          });
+    } 
+};
 
-const productoCtrl={};
-
-productoCtrl.listarProductos=(req, res)=>{
-    //cargar la logica para traer los productos de la BD    
-    res.send("aqui enviaria la lista de productos");
-
-}
-
-productoCtrl.crearProducto=(req, res)=>{
-    console.log(req.body);
-    res.send('doy el alta de un producto');
-}
+productoCtrl.crearProducto = async(req, res) => {
+  try {
+     //validar el (req.body) copiadas del front
+     //crear un obj para guardar en la BBDD
+     const newProd= new Producto({
+         productName: req.body.productName,
+         price: req.body.price,
+         urlImg: req.body.urlImg,
+         category: req.body.category
+     });
+     //una vez creado se guarda en la BBDD
+     await newProd.save();
+     //enviar resp al front end
+     res.status(201).json({
+         mensaje: 'El producto se creo correctamente'
+     })
+  } catch (error) {
+    console.log(error);
+    //envio de respuesta al front
+    res.status(404).json({
+      mensaje: "Error al intentar agregar un producto",
+    });
+  }
+};
 
 export default productoCtrl;
--- src/models/producto.js
+++ src/models/producto.js
@@ -2,3 +2,35 @@
 
 //los datos se guardan en documentos y estos a su vez en colecciones
 
+//el Schema es el que permite modelar
+
+import mongoose, {Schema} from 'mongoose';
+
+const prodSchema= new Schema({
+      productName:{
+          type: String,
+          required: true,
+          unique: true,
+          maxlength: 150
+      },
+      price:{
+          type: Number,
+          required: true                  
+      },
+      urlImg:{
+          type: String,
+          required: true
+      },
+      category:{
+          type: String,
+          required: true          
+      }
+
+});
+
+//convertir en modelo
+//esta constante es la representacion de toda la BBDD
+//primer param, nombre de los doc y colecciones, segundo param shema
+const Producto = mongoose.model('producto', prodSchema);
+
+export default Producto;
