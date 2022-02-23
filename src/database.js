7d8b7fe8eced61685f2a399c0b80adf304753687
alanChibilisco <alan.chibilisco@gmail.com>
2022-02-23 18:26:54 -0300

    se modifico en database la cadena de conexion

--- src/database.js
+++ src/database.js
@@ -1,6 +1,7 @@
 import  mongoose  from "mongoose";
 
-const url='mongodb://localhost:27017/bdcafe';
+//const url='mongodb://localhost:27017/bdcafe';
+const url="mongodb+srv://cloudubuntu:OyKBdJ7FJMQX250L@cluster0.4jhlh.mongodb.net/bdcafe";
 
 mongoose.connect(url);
 
